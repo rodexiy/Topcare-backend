3b2ea5005afb1bd0a9c579df10e82803146ac7d4
DANIEL LUÍS DA ROCHA <daniel_luis-rocha@estudante.sesisenai.org.br>
2024-07-29 16:20:40 -0300

    dev

--- src/main/java/net/weg/topcare/repository/ProductRepository.java
+++ src/main/java/net/weg/topcare/repository/ProductRepository.java
@@ -2,8 +2,15 @@ package net.weg.topcare.repository;
 
 import net.weg.topcare.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Query("SELECT p FROM Product p WHERE p.name LIKE %?1%"
+            + " OR p.brand.name LIKE %?1%"
+            + " OR CONCAT(p.price, '') LIKE %?1%")
+    public List<Product> search(String keyword);
 }
