6b5211ebf1be633fed5251d3cfa9b07a8fa7fb31
enzoCorrea123 <enzorufino1508@gmail.com>
2024-09-18 16:44:09 -0300

    rota

--- src/main/java/net/weg/topcare/controller/EmployeeController.java
+++ src/main/java/net/weg/topcare/controller/EmployeeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/funcionarios")
+@RequestMapping("/api/funcionarios")
 @RequiredArgsConstructor
 public class EmployeeController {
     private final EmployeeServiceImpl service;
