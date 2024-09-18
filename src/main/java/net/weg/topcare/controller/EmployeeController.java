9a3f9fe0059e584ee11137bb946812130cf2d054
enzoCorrea123 <enzorufino1508@gmail.com>
2024-09-18 16:11:30 -0300

    controller

--- src/main/java/net/weg/topcare/controller/EmployeeController.java
+++ src/main/java/net/weg/topcare/controller/EmployeeController.java
@@ -1,11 +1,21 @@
 package net.weg.topcare.controller;
 
+import net.weg.topcare.controller.dto.employee.PostEmployeeDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/funcionarios")
 public class EmployeeController {
+    private final EmployeeServiceImpl service;
+    @PostMapping
+    public ResponseEntity<PostEmployeeDto> postEmployee(@RequestBody PostEmployeeDto dto){
+        return new ResponseEntity<>(service.register(dto), HttpStatus.OK);
 
+    }
 
 }
