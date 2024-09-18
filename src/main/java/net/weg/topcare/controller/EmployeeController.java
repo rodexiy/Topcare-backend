615275495a889e5f4cd03148322826400f87da1d
enzoCorrea123 <enzorufino1508@gmail.com>
2024-09-18 16:09:16 -0300

    dto

---
+++ src/main/java/net/weg/topcare/controller/EmployeeController.java
@@ -0,0 +1,11 @@
+package net.weg.topcare.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/funcionarios")
+public class EmployeeController {
+
+
+}
